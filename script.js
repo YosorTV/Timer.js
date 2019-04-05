d754ab4669a5b8705727af9978b3da9030d2531f
YosorIT <69yoyeezus69@gmail.com>
2019-04-05 13:26:59 +0300

    timer constructor

---
+++ script.js
@@ -0,0 +1,95 @@
+class State {
+    constructor(startTimestamp, difference, suspended) {
+        this.startTimestamp = startTimestamp;
+        this.difference = difference;
+        this.suspended = suspended;
+    }
+
+    static ready() {
+        return new State(null, 0, 0);
+    }
+}
+
+class Stopwatch {
+    constructor(state) {
+        this.state = state;
+        this.requestAnimationId = null;
+        this.handleClickStart = this.handleClickStart.bind(this);
+        document.querySelector(".start").addEventListener("click", this.handleClickStart);
+        this.handleClickStop = this.handleClickStop.bind(this);
+        document.querySelector(".stop").addEventListener("click", this.handleClickStop);
+        this.handleClickReset = this.handleClickReset.bind(this);
+        document.querySelector(".reset").addEventListener("click", this.handleClickReset);
+        this.tick = this.tick.bind(this);
+        this.render();
+    }
+
+    static ready() {
+        return new Stopwatch(State.ready());
+    }
+
+    setState(newState) {
+        this.state = {
+            ...this.state,
+            ...newState
+        };
+        this.render();
+    }
+
+    tick() {
+        this.setState({
+            difference: new Date(new Date() - this.state.startTimestamp)
+        });
+        this.requestAnimationId = requestAnimationFrame(this.tick);
+    }
+
+    handleClickStart() {
+        if (this.state.startTimestamp) {
+            // Prevent multi clicks on start
+            return;
+        }
+        this.setState({
+            startTimestamp: new Date() - this.state.suspended,
+            suspended: 0
+        });
+        this.requestAnimationId = requestAnimationFrame(this.tick);
+    }
+
+    handleClickStop() {
+        cancelAnimationFrame(this.requestAnimationId);
+        this.setState({
+            startTimestamp: null,
+            suspended: this.state.difference
+        });
+    }
+
+    handleClickReset() {
+        cancelAnimationFrame(this.requestAnimationId);
+        this.setState(State.ready());
+    }
+
+    render() {
+        const {
+            difference
+        } = this.state;
+        const millieseconds = (difference ?
+                Math.floor(difference.getMilliseconds() / 10) :
+                0
+            )
+            .toString()
+            .padStart(2, "0");
+        const seconds = (difference ? Math.floor(difference.getSeconds()) : 0)
+            .toString()
+            .padStart(2, "0");
+        const minutes = (difference ? Math.floor(difference.getMinutes()) : 0)
+            .toString()
+            .padStart(2, "0");
+
+        // Render screen
+        document.querySelector(".minutes").textContent = minutes;
+        document.querySelector(".seconds").textContent = seconds;
+        document.querySelector(".millieseconds").textContent = millieseconds;
+    }
+}
+
+const Timer = Stopwatch.ready();
